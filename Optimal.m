0be3821bfa327f292456d2a4d767ad2034f6d4c2
Dr.Mohamed Mustafa <mabberer@gmail.com>
2025-01-06 20:52:42 +0300

    edited

--- Optimal.m
+++ Optimal.m
@@ -1,5 +1,5 @@
 clc; clear; close all;
-h= 0.01; t(1)=0; tfinal=300; t=t(1):h:tfinal; N=ceil((tfinal-t(1))/h); % Inputs
+h= 0.1; t(1)=0; tfinal=300; t=t(1):h:tfinal; N=ceil((tfinal-t(1))/h); % Inputs
 S(1) = 2.67e7;
 V(1) = 9.61e6;
 E(1) = 7;
@@ -12,22 +12,22 @@ alpha=0.70; % this is fractional order
 tau=1; % this is fractal dimension
 sigma1 = 0.114;   % Recruitment rate of individuals into the population
 sigma2 = 0.000256; % Natural death rate
-sigma3 = 0.858;   % Average effective contact rate
+sigma3 = 0.958;   % Average effective contact rate
 sigma4 = 0.114;   % Vaccination rate
 sigma5 = 1.00;    % Vaccine inefficacy
 sigma6 = 0.0015;  % Average hospitalization rate
-sigma7 = 2.6;     % Recovery rate for infected individuals
+sigma7 = 1.76;     % Recovery rate for infected individuals
 sigma8 = 0.34;    % Recovery rate for exposed individuals
-sigma9 = 0.68;    % Recovery rate for hospitalized individuals
+sigma9 = 0.092;    % Recovery rate for hospitalized individuals
 sigma10 = 1 / 1.6;% Average latent or incubation rate (days^-1)
 sigma11 = 1 / 50; % Rate at which individuals lose immunity (days^-1)
 % The Model (3.4) in the Research Paper
-f1 = @(t,S,V,E,I,H,R) h^alpha * (sigma1 + sigma11 *(V + R) -sigma3 *(E+I).*S / total_pop - sigma2*S);
-f2 = @(t,S,V,E,I,H,R)  h^alpha * (sigma4 * S-sigma5 * sigma3 * (E+I).*V / total_pop-(sigma11 + sigma2)*V);
-f3 = @(t,S,V,E,I,H,R) h^alpha * (sigma3 * (E+I).*S / total_pop - (sigma10 + sigma2)*E);
-f4 = @(t,S,V,E,I,H,R) h^alpha * (sigma10*E - (sigma8 + sigma2)*I);
-f5 = @(t,S,V,E,I,H,R) h^alpha * (sigma8 * I - (sigma6 + sigma9 + sigma2)*H);
-f6 = @(t,S,V,E,I,H,R) h^alpha * (sigma6*H-(sigma7 + sigma2)*R);
+f1 = @(t,S,V,E,I,H,R) sigma1 + sigma11 *(V + R) -sigma3 *(E+I).*S / total_pop - (sigma4+sigma2)*S;
+f2 = @(t,S,V,E,I,H,R) sigma4*S-sigma5 * sigma3 * (E+I).*V / total_pop-(sigma11 + sigma2)*V;
+f3 = @(t,S,V,E,I,H,R) sigma3 * (E+I).*S / total_pop - (sigma10 + sigma2)*E;
+f4 = @(t,S,V,E,I,H,R) sigma10*E - (sigma8 + sigma2)*I;
+f5 = @(t,S,V,E,I,H,R) sigma8 * I - (sigma6 + sigma9 + sigma2)*H;
+f6 = @(t,S,V,E,I,H,R) sigma6*H-(sigma7 + sigma2)*R;
 % Algorithm of the Caputo Fractal-Fractional starts
 for n=1:N
     j=2:n;
